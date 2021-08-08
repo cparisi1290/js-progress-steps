1f82e6e2e6c5aee0e679bce2d6aeae847ac72e3f
Cassandra <cparisi1290@gmail.com>
2021-08-08 07:19:22 -0500

    modify css for width of progress line to move only to next circled number by changing percent of progress

--- script.js
+++ script.js
@@ -37,4 +37,9 @@ function update() {
       circle.classList.remove('active');
     }
   });
+
+  const actives = document.querySelectorAll('.active');
+
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + '%';
 }
